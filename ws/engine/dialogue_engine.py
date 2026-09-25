1ea6fcfb0910777b45fd6fca83b76c82100173e1
txjws05-debug <txjws05>
2026-09-25 22:18:52 +0800

    feat :Implement dialogue engine text/object handling and task plan validation

--- ws/engine/dialogue_engine.py
+++ ws/engine/dialogue_engine.py
@@ -1,6 +1,13 @@
 import time
 import uuid
+from dataclasses import asdict
 
+
+from domain.state import FocusedObject
+from plan.models import TurnPlanValidationResult
+from task.command.models import SetSlotsCommand
+from task.flow.models import Flow
+from task.flow.steps import FlowStep, CollectSlotStep
 from ws.domain.message import UserMessage, ProcessResult, MessageType, BotMessage
 from ws.domain.state import DialogueState, Turn
 from ws.plan import turn_plan
@@ -65,27 +72,88 @@ class DialogueEngine:
                 #更新
                 current_session.last_activity_at=now
     #处理文本类型消息
-    def _execute_text_message(self,user_message:UserMessage,state:DialogueState)->list[BotMessage]:
+    async def _execute_text_message(self,user_message:UserMessage,state:DialogueState)->list[BotMessage]:
         #1根据user——message文本提问信息，调用llm，进行意图识别
         #识别执行哪个轨道：任务流程、知识检索、闲聊
         #如果任务流程，识别流程id
-
+        turnPlan:TurnPlan = await self._turn_plan.plan(user_message=user_message,
+                                                       state=state,
+                                                       flow_catalog=self.task_handler._flow_catalog)
         #2 对llm意图识别结果校验
         ##如有两个轨道，任务流程识别流程id不存在
-
+        validation:TurnPlanValidationResult=self._turn_plan_validation.validate(turn_plan=turnPlan,
+                                                                                state=state,
+                                                                                flow_catalog=self._task_handler._flow_catalog)
 
         #校验失败，调用反问澄清组件
-
+        if not validation.valid:
+            #todo 反问澄清插件
+            pass
         #校验成功，根据识别不同轨道，调用不同handler处理
         #比如识别任务流程调用TaskHandelr方法执行
-        
+        if turnPlan.task:
+            return  await self.task_handler.handle(
+                commands=turnPlan.task.command,
+                state=state,
+                user_message=user_message
+            )
+        if turnPlan.knowledge:
+            pass
+        if  turnPlan.chitchat:
+            pass
 
-        pass
 
      #处理对象类型消息
-    def _execute_object_message(self,user_message,state):
-        pass
+    async def _execute_object_message(self,user_message,state):
+        #1把对象消息放到state里面focused_object
+        state.share.focuse_object=FocusedObject(
+            **asdict(user_message.object)
+        )
+        #判断是否填充槽位数据
+        if self._can_fill_slots(state):
+            if user_message.object.type=='order':
+                slots={'order_number':user_message.object.id}
+            else :
+                slots={'product_id':user_message.object.id}
+        # 最终调TaskHandler里面方法，传入command对象，对象类型消息处理
+        #没有调用意图识别组件，没有command
+        #手动构建command对象设置对应类型
+        #{"command":"set_slots",{"<slot_name>":"<value>"}}
+            command=SetSlotsCommand(
+            command='set_slots',
+            slots=slots
+        )
+            #调用TaskHandler方法执行
+            return await self.task_handler.handle(
+            command=[command],
+            state=state,
+            user_message=user_message,
+        )
+        else:
+            # 反问澄清
+            pass
+    def _can_fill_slots(self,state:DialogueState) -> bool:
+        #1判断当前是否有活跃任务
+        active_task=state.tasks.active
+        if not active_task:
+            return False
+        #有活跃任务
+        #根据当前任务流程id，获取流程对象
+        flow_id= active_task.flow_id
+        flow:Flow =self.task_handler._flow_catalog.get_flow_by_id(flow_id)
 
+        #从流程对象获取所有步骤列表，当前任务步骤id到列表找到步骤对应数据
+        step:FlowStep=flow.get_step_by_id(active_task.step_id)
+        #判断当前步骤是否collect类型
+        if not isinstance(step,CollectSlotStep):
+            return False
+
+        if(step.solt_name=='order_number') and(state.share.focuse_object.type=='order'):
+            return True
+
+        if (step.solt_name=='product_id')and (state.share.focuse_object.type=='product'):
+            return True
+        return False
 
 
 
--- ws/plan/turn_plan_validation.py
+++ ws/plan/turn_plan_validation.py
@@ -1,3 +1,5 @@
+from plan.models import TaskTurnPlan
+from task.command.models import StartFlowCommand, ResumeTaskCommand, CancelTaskCommand
 from ws.domain.state import DialogueState
 from ws.plan.models import TurnPlan, TurnPlanValidationResult, ClarifyReason
 from ws.task.flow.models import FlowCatalog
@@ -37,9 +39,52 @@ class TurnPlanValidation:
             self._validate_knowledge_plan()
         return TurnPlanValidationResult(valid=True)
     #对task意图识别
-    def _validate_task_plan(self):
+    def _validate_task_plan(self,
+                            task:TaskTurnPlan,
+                            state: DialogueState,
+                            flow_catalog:FlowCatalog
+                                ):
         #根据不同类型的command做不同检验
-        pass
+        for command in task.commands:
+            #1 start_flow 校验flow_id是否存在于当前流程里面yaml里面
+            if isinstance(command,StartFlowCommand):
+                #获取start_flow流程id
+                flow_id =command.flow
+                #校验flow_id是否存在于当前流程里面yaml里面
+                if flow_id not in flow_catalog.flows:
+                    return  TurnPlanValidationResult(
+                        valid=False,
+                        reason=ClarifyReason.INVALID_TASK_COMMAND
+                    )
+            #2 resume_task校验task_id 是否存在于中断列表
+            if isinstance(command,ResumeTaskCommand):
+                #判断回复任务id在中断列表是否存在
+                #获取中断列表索引任务id
+                #[1,2,3]
+                paused_task_ids=[paused_task.task_id
+                                 for paused_task in state.tasks.paused]
+                #当前command任务id和所有中断列表任务id比较
+                if command.task_id not in paused_task_ids:
+                    return TurnPlanValidationResult(
+                        valid=False,
+                        reason=ClarifyReason.INVALID_TASK_COMMAND
+                    )
+            #3 cancal_task 校验task_id 是否存在于 中断列表 或者当前任务里面
+            if  isinstance(command,CancelTaskCommand):
+                #获取中断列表任务所有id
+                all_task_ids=[paused_task.task_id
+                              for paused_task in state.tasks.paused  ]
+                #获取当前活跃任务id
+                if state.tasks.active:
+                    all_task_ids.append(state.tasks.active.task_id)
+                #判断
+                if command.task_id not in all_task_ids:
+                    return  TurnPlanValidationResult(
+                        valid=False,
+                        reason=ClarifyReason.INVALID_TASK_COMMAND
+                    )
+
+        return TurnPlanValidationResult(valid=True)
 
     def _validate_knowledge_plan(self):
         pass
